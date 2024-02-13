e0ffc84025ae41ac742f4d72ff022c0224ef3d77
ProgressAce <daiyaansallie7@gmail.com>
2024-02-14 00:21:44 +0200

    add custom error handler for 403 status codes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,7 @@
 Route module for the API
 """
 from os import getenv
+from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
@@ -12,6 +13,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 
 @app.errorhandler(401)
@@ -20,13 +22,40 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden request handler"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
+@app.before_request
+def filter_request_beforehand():
+    """Filters each request before passing it as route."""
+
+    if auth:
+        if not auth.require_auth(
+            request.path,
+            ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"],
+        ):
+            if auth.authorization_header(request) is None:
+                abort(401)
+
+            if auth.current_user(request) is None:
+                abort(403)
+
+
 if __name__ == "__main__":
+    # determine the authentication type
+    auth_type = getenv("AUTH_TYPE")
+    if auth_type == "auth":
+        auth = Auth()
+
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
