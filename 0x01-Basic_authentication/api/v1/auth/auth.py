e96138d02a0867939339b4f10436917b02ef16c6
ProgressAce <daiyaansallie7@gmail.com>
2024-02-14 00:19:05 +0200

    add to and test require_auth and authorization_header methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,20 +1,47 @@
 #!/usr/bin/
 """Manages API authentication."""
 
-from Flask import 
+from flask import request as req
 from typing import List, TypeVar
+
 
 class Auth:
     """Handles API authentication."""
 
     def require_auth(self, path: str, exluded_paths: List[str]) -> bool:
-        """doc"""
+        """Defines which routes don't need authentication.
+
+        It is assumed that <excluded_paths> always contains string paths
+        ended by '/'.
+
+        Args:
+            path: the route
+            excluded_paths: the paths which don't require authentication.
+        """
+
+        # checking for empty arguments
+        if path is None:
+            return True
+
+        if exluded_paths is None or len(exluded_paths) == 0:
+            return True
+
+        # slash tolerant path (can be /api/v1/stats or /api/v1/stats/)
+        slash_path = path + "/" if path[-1] != "/" else path
+
+        if slash_path not in exluded_paths:
+            return True
+
         return False
-    
+
     def authorization_header(self, request=None) -> str:
-        """doc"""
-        return None
-    
-    def current_user(self, request=None) -> TypeVar('User'):
+        """Validates requests to secure the API."""
+
+        if request is None or not request.get("Authorization"):
+            return None
+
+        return request.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar("User"):
         """doc"""
         return None
