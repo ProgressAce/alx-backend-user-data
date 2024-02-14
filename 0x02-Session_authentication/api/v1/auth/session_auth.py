8faceff4940eeb1fd0f0f13cb5731e1c39e05321
ProgressAce <daiyaansallie7@gmail.com>
2024-02-14 17:31:49 +0200

    define and test <create_session> method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 """Implements a SessionAuth authentication system."""
 
+import uuid
 from api.v1.auth.auth import Auth
+from typing import Dict
 
 
 class SessionAuth (Auth):
     """Defines the SessionAuth authentication mechanism."""
+
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a <user_id>.
+
+        Arg:
+            user_id: string id of a user."""
+
+        if user_id is None or type(user_id) != str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
