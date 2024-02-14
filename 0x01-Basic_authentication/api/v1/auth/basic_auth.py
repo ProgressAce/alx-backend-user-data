23fa8c673dd3ffec875f9a6f7d1adc9596e79b6c
ProgressAce <daiyaansallie7@gmail.com>
2024-02-14 11:00:58 +0200

    add and test extract_user_credentials method & include exception for decode_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Implements a Basic Authentication system."""
 
 import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -42,7 +43,32 @@ class BasicAuth(Auth):
 
         try:
             decoded_base64 = base64.b64decode(base64_authorization_header)
-        except:
+        except binascii.Error:
             return None
 
         return decoded_base64.decode("utf-8")
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Returns the user email and password from the Base64 decoded value.
+
+        It is assumed that <decoded_base64_authorization_header> will
+        only contain one ':'.
+
+        Arg:
+            decoded_base64_authorization_header: the decoded base64 value."""
+
+        no_details = (None, None)
+
+        if decoded_base64_authorization_header is None:
+            return no_details
+        if not isinstance(decoded_base64_authorization_header, str):
+            return no_details
+        if ':' not in decoded_base64_authorization_header:
+            return no_details
+
+        # seperate email and password parts from the decoded base64 string
+        split_str = decoded_base64_authorization_header.split(':')
+
+        return (split_str[0], split_str[1])
