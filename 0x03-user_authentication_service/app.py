e2492da49951ab7768c3dca4513eb3cf489513db
ProgressAce <daiyaansallie7@gmail.com>
2024-02-20 10:25:27 +0200

    define route for registering a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Web server Flask set up."""
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
+from typing import Dict
 
-app = Flask(__name__)
+AUTH = Auth()
+app: Flask = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,6 +15,40 @@ def index():
 
     response - message: Bienvenue"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user_registration():
+    """ POST /users
+    JSON body:
+      - email
+      - password
+    Return:
+      if user already registered, a JSON represented informing message.
+      code 400
+      if user does not exist, a JSON represented confirmation message.
+      code 200
+    """
+
+    r_json: Dict = None
+    error_msg: str = None
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # TODO: validate form data
+    # use/return them for more detailed error messages
+
+    try:
+        user = AUTH.register_user(email, password)
+
+        # None is returned if the given arguments/form data were invalid
+        if not user:
+            return jsonify({"message": "missing email and/or password"})
+
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == '__main__':
