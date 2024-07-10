cda163132dd2c866d6e0515059c52f138a8efd2c
ProgressAce <daiyaansallie7@gmail.com>
2024-07-10 18:49:11 +0200

    feat: define session auth system

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Session authentication system
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session Authentication mechanism of the app.
+    """
+    pass
