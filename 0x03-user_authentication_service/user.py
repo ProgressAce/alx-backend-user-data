6bbcbef0da4580bd5e5ab1feb8f127e4feec782a
ProgressAce <daiyaansallie7@gmail.com>
2024-07-15 12:44:53 +0200

    add: user model
    The user model that describes the user table in the database

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+User model for end-users.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
