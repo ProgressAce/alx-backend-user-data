42bdef5bea9132dfc63287b29dda5832a9a89e0a
ProgressAce <daiyaansallie7@gmail.com>
2024-07-16 07:32:10 +0200

    feat: find user in database
    The `find_user_by` method of DB searches for a user based on the keyworded arguments provided. Since the query ignore invalid arguments and returns none when no results are found, the SQLAlchemy exceptions InvalidRequestError and NoResultFound are raised when an their tested conditions arise.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -39,5 +41,32 @@ class DB:
         user: User = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Looks for users in the database that match the requirements.
+        No validation of input arguments are done yet.
+
+        Only the FIRST user is returned. The kwargs are used as the filter
+        criteria.
+        Raises:
+          - InvalidRequestError: when any keyworded argument is invalid
+          (not an attribute of user).
+          - NoResultFound: when a user is not found with the database query.
+        Returns:
+          - the first record of the matched query for the user table.
+        """
+        valid_keys = ['id', 'email', 'hashed_password',
+                      'session_id', 'reset_token']
+
+        for key in kwargs.keys():
+            if key not in valid_keys:
+                raise InvalidRequestError
+
+        user: User = self._session.query(User).filter_by(**kwargs).first()
+
+        if not user:
+            raise NoResultFound
 
         return user
