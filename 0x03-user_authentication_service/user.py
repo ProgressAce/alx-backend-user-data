5578ec3da4ec21d0df47f9e73f138b9116d47104
ProgressAce <daiyaansallie7@gmail.com>
2024-07-18 20:25:07 +0200

    doc: add more class and model description

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,10 +9,11 @@ Base = declarative_base()
 
 
 class User(Base):
+    """Representation of an end-user."""
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(70), nullable=True)
+    reset_token = Column(String(70), nullable=True)
